3ae39329f37aa59d5824386375c9bf399e601b00
Mister-Star <745470728@qq.com>
2023-02-15 00:48:50 -0800

    two-server debug 2-15-4

--- src/message/handler_receive.cpp
+++ src/message/handler_receive.cpp
@@ -115,7 +115,7 @@ namespace Taas {
                 break;
             }
             case proto::TxnType::RemoteServerTxn : {
-                printf("receive remote txn\n");
+//                printf("receive remote txn\n");
                 if ((EpochManager::GetLogicalEpoch() % ctx.kCacheMaxLength) ==
                     ((message_epoch_mod + 55) % ctx.kCacheMaxLength))
                     assert(false);
@@ -124,7 +124,7 @@ namespace Taas {
                 break;
             }
             case proto::TxnType::EpochEndFlag : {
-                printf("receive remote end flag server_id %lu epoch %lu sharding_id %lu\n", message_server_id, message_epoch, message_sharding_id);
+//                printf("receive remote end flag server_id %lu epoch %lu sharding_id %lu\n", message_server_id, message_epoch, message_sharding_id);
                 sharding_should_receive_txn_num.IncCount(message_epoch,message_server_id,txn_ptr->csn());
                 sharding_received_pack_num.IncCount(message_epoch,message_server_id, 1);
                 break;
@@ -132,7 +132,7 @@ namespace Taas {
             case proto::CommittedTxn:
                 break;
             case proto::TxnType::BackUpTxn : {
-                printf("receive remote backup txn\n");
+//                printf("receive remote backup txn\n");
                 if ((EpochManager::GetLogicalEpoch() % ctx.kCacheMaxLength) ==
                     ((message_epoch_mod + 55) % ctx.kCacheMaxLength))
                     assert(false);
@@ -141,36 +141,36 @@ namespace Taas {
                 break;
             }
             case proto::TxnType::BackUpEpochEndFlag : {
-                printf("receive remote backup end flag server_id %lu epoch %lu sharding_id %lu\n", message_server_id, message_epoch, message_sharding_id);
+//                printf("receive remote backup end flag server_id %lu epoch %lu sharding_id %lu\n", message_server_id, message_epoch, message_sharding_id);
                 backup_should_receive_txn_num.IncCount(message_epoch,message_server_id,txn_ptr->csn());
                 backup_received_pack_num.IncCount(message_epoch,message_server_id, 1);
                 break;
             }
             case proto::TxnType::AbortSet : {
-                printf("receive abort\n");
+//                printf("receive abort\n");
                 UpdateEpochAbortSet();
                 sharding_received_abort_set_num.IncCount(message_epoch,message_server_id, 1);
                 abort_set_cache[message_epoch_mod][message_server_id]->push(std::move(txn_ptr));
                 break;
             }
             case proto::TxnType::InsertSet : {
-                printf("receive insert \n");
+//                printf("receive insert \n");
                 insert_set_received_num.IncCount(message_epoch,message_server_id, 1);
                 insert_set_cache[message_epoch_mod][message_server_id]->push(std::move(txn_ptr));
                 break;
             }
             case proto::TxnType::BackUpACK : {
-                printf("receive backup ack\n");
+//                printf("receive backup ack\n");
                 backup_received_ack_num.IncCount(message_epoch,message_server_id, 1);
                 break;
             }
             case proto::TxnType::AbortSetACK : {
-                printf("receive abort ack\n");
+//                printf("receive abort ack\n");
                 sharding_abort_set_received_ack_num.IncCount(message_epoch,message_server_id, 1);
                 break;
             }
             case proto::TxnType::InsertSetACK : {
-                printf("receive insert ack\n");
+//                printf("receive insert ack\n");
                 insert_set_received_ack_num.IncCount(message_epoch,message_server_id, 1);
                 break;
             }
@@ -306,21 +306,21 @@ namespace Taas {
                     backup_should_receive_txn_num.GetCount(backup_send_ack_epoch_num[server_reply_ack_id], server_reply_ack_id)
             ) {
                 ///send reply message
-                printf("send backup ack server_id %lu epoch %lu sharding_id %lu\n", server_reply_ack_id, backup_send_ack_epoch_num[server_reply_ack_id], server_reply_ack_id);
+//                printf("send backup ack server_id %lu epoch %lu sharding_id %lu\n", server_reply_ack_id, backup_send_ack_epoch_num[server_reply_ack_id], server_reply_ack_id);
                 MessageSendHandler::SendTaskToPackThread(ctx, backup_send_ack_epoch_num[server_reply_ack_id],
                                                         server_reply_ack_id, proto::TxnType::BackUpACK);
                 backup_send_ack_epoch_num[server_reply_ack_id] ++;
                 res = true;
             }
             if(sharding_received_abort_set_num.GetCount(backup_insert_set_send_ack_epoch_num[server_reply_ack_id], server_reply_ack_id) > 0) {
-                printf("send abort set ack\n");
+//                printf("send abort set ack\n");
                 MessageSendHandler::SendTaskToPackThread(ctx, backup_insert_set_send_ack_epoch_num[server_reply_ack_id],
                                                         server_reply_ack_id, proto::TxnType::AbortSetACK);
                 backup_insert_set_send_ack_epoch_num[server_reply_ack_id] ++;
                 res = true;
             }
             if(insert_set_received_num.GetCount(abort_set_send_ack_epoch_num[server_reply_ack_id], server_reply_ack_id) > 0) {
-                printf("send insert set ack\n");
+//                printf("send insert set ack\n");
                 MessageSendHandler::SendTaskToPackThread(ctx, abort_set_send_ack_epoch_num[server_reply_ack_id],
                                                         server_reply_ack_id, proto::TxnType::InsertSetACK);
                 abort_set_send_ack_epoch_num[server_reply_ack_id] ++;
--- src/message/handler_send.cpp
+++ src/message/handler_send.cpp
@@ -114,27 +114,27 @@ namespace Taas {
                         break;
                     }
                     case proto::TxnType::AbortSet : {
-                        printf("pack abort set\n");
+//                        printf("pack abort set\n");
                         SendAbortSet(id, ctx, pack_param->epoch);
                         break;
                     }
                     case proto::TxnType::InsertSet : {
-                        printf("pack Insert set\n");
+//                        printf("pack Insert set\n");
                         SendInsertSet(id, ctx, pack_param->epoch);
                         break;
                     }
                     case proto::TxnType::BackUpACK : {
-                        printf("pack backup ack\n");
+//                        printf("pack backup ack\n");
                         SendACK(id, ctx, pack_param->epoch, pack_param->id, pack_param->type);
                         break;
                     }
                     case proto::TxnType::AbortSetACK : {
-                        printf("pack AbortSetACK\n");
+//                        printf("pack AbortSetACK\n");
                         SendACK(id, ctx, pack_param->epoch, pack_param->id, pack_param->type);
                         break;
                     }
                     case proto::TxnType::InsertSetACK : {
-                        printf("pack InsertSetACK\n");
+//                        printf("pack InsertSetACK\n");
                         SendACK(id, ctx, pack_param->epoch, pack_param->id, pack_param->type);
                         break;
                     }
@@ -273,7 +273,7 @@ namespace Taas {
         auto serialized_txn_str_ptr = std::make_unique<std::string>();
         auto res = Gzip(msg.get(), serialized_txn_str_ptr.get());
         assert(res);
-        printf("send ack to %lu epoch %lu type %lu\n", to_whom, send_epoch, txn_type);
+//        printf("send ack to %lu epoch %lu type %lu\n", to_whom, send_epoch, txn_type);
         send_to_server_queue.enqueue(std::move(std::make_unique<send_params>(to_whom, 0, "", send_epoch, txn_type, std::move(serialized_txn_str_ptr), nullptr)));
         send_to_server_queue.enqueue(std::move(std::make_unique<send_params>(to_whom, 0, "", send_epoch, proto::TxnType::NullMark, nullptr, nullptr)));
         send_epoch ++;
