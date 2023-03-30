01f0e375a643f720709164238d2f12b908a42acc
zwx-vm2 <745470728@qq.com>
2023-03-30 23:12:12 +0800

    12

--- src/message/message_transmit_servers.cpp
+++ src/message/message_transmit_servers.cpp
@@ -30,16 +30,13 @@ namespace Taas {
         zmq::message_t reply(5);
         zmq::send_flags sendFlags = zmq::send_flags::none;
         int queue_length = 0;
-        std::unordered_map<std::uint64_t, std::unique_ptr<zmq::socket_t>> socket_map;
+        std::unordered_map<std::uint64_t, std::unique_ptr<util::ZMQInstance>> socket_map;
         std::unique_ptr<send_params> params;
-        std::unique_ptr<zmq::message_t> msg;
         for (int i = 0; i < (int) ctx.kServerIp.size(); i++) {
             if (i == (int) ctx.txn_node_ip_index) continue;
-            auto socket = std::make_unique<zmq::socket_t>(context, ZMQ_PUSH);
-            socket->set(zmq::sockopt::sndhwm, queue_length);
-            socket->set(zmq::sockopt::rcvhwm, queue_length);
-            socket->connect("tcp://" + ctx.kServerIp[i] + ":" + std::to_string(20000+i));
-            socket_map[i] = std::move(socket);
+            auto ret = util::ZMQInstance::NewClient<zmq::socket_type::push>(ctx.kServerIp[i], 20000+i);
+            CHECK(ret != nullptr);
+            socket_map[i] = std::move(ret);
             printf("Send Server connect ZMQ_PUSH %s", ("tcp://" + ctx.kServerIp[i] + ":" + std::to_string(20000+i) + "\n").c_str());
         }
         printf("线程开始工作 SendServerThread\n");
@@ -48,11 +45,9 @@ namespace Taas {
             send_to_server_queue->wait_dequeue(params);
             if (params == nullptr || params->type == proto::TxnType::NullMark) continue;
             if(params->id == ctx.txn_node_ip_index) assert(false);
-            msg = std::make_unique<zmq::message_t>(*(params->str));
             printf("send a message type %d\n", (params->type));
-            socket_map[params->id]->send(*msg, sendFlags);
+            socket_map[params->id]->send(std::move(*(params->str)));
         }
-        socket_map[0]->send((zmq::message_t &) "end", sendFlags);
     }
 
 /**
@@ -65,18 +60,18 @@ namespace Taas {
         SetCPU();
         // 设置ZeroMQ的相关变量，监听其他txn node是否有写集发来
         zmq::context_t listen_context(1);
-        zmq::socket_t socket_listen(listen_context, ZMQ_PULL);
         zmq::recv_flags recvFlags = zmq::recv_flags::none;
         zmq::recv_result_t  recvResult;
         int queue_length = 0;
-        socket_listen.bind("tcp://*:" + std::to_string(20000+ctx.txn_node_ip_index));//to server
-        socket_listen.set(zmq::sockopt::sndhwm, queue_length);
-        socket_listen.set(zmq::sockopt::rcvhwm, queue_length);
+        auto server = util::ZMQInstance::NewServer<zmq::socket_type::pull>(20000+ctx.txn_node_ip_index);
+        CHECK(server != nullptr);
         printf("线程开始工作 ListenServerThread ZMQ_PULL tcp://*:%s\n", std::to_string(20000+ctx.txn_node_ip_index).c_str());
         while(!EpochManager::IsInitOK()) usleep(1000);
         while (!EpochManager::IsTimerStop()) {
-            std::unique_ptr<zmq::message_t> message_ptr = std::make_unique<zmq::message_t>();
-            recvResult = socket_listen.recv((*message_ptr), recvFlags);//防止上次遗留消息造成message cache出现问题
+            auto ret = server->receive();
+            CHECK(ret != std::nullopt);
+            std::unique_ptr<zmq::message_t> message_ptr = std::make_unique<zmq::message_t>(std::move(*ret));
+            printf("receive a message\n");
             if(recvResult < 0) assert(false);
             if (is_epoch_advance_started.load()) {
                 if (!listen_message_queue->enqueue(std::move(message_ptr))) assert(false);
@@ -87,8 +82,9 @@ namespace Taas {
         }
 
         while (!EpochManager::IsTimerStop()) {
-            std::unique_ptr<zmq::message_t> message_ptr = std::make_unique<zmq::message_t>();
-            recvResult = socket_listen.recv((*message_ptr), recvFlags);
+            auto ret = server->receive();
+            CHECK(ret != std::nullopt);
+            std::unique_ptr<zmq::message_t> message_ptr = std::make_unique<zmq::message_t>(std::move(*ret));
             if(recvResult < 0) assert(false);
             printf("receive a message\n");
             if (!listen_message_queue->enqueue(std::move(message_ptr))) assert(false);
