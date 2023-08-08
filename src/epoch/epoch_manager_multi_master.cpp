0b01002c33e37e26cc24a56711f192fa5b18264a
zwx-vm2 <745470728@qq.com>
2023-08-08 11:00:54 +0800

    debug merge epoch 1000

--- src/epoch/epoch_manager_multi_master.cpp
+++ src/epoch/epoch_manager_multi_master.cpp
@@ -82,32 +82,56 @@ namespace Taas {
     void MultiMasterEpochManager::EpochLogicalTimerManagerThreadMain(const Context& ctx) {
         while(!EpochManager::IsInitOK()) usleep(sleep_time);
         uint64_t epoch = 1, cnt = 0;
-        OUTPUTLOG(ctx, "===== Start Epoch的合并 ===== ", epoch);
+        OUTPUTLOG(ctx, "===== Logical Start Epoch的合并 ===== ", epoch);
         while(!EpochManager::IsInitOK()) usleep(logical_sleep_timme);
         if(ctx.kTxnNodeNum > 1) {
             while(!EpochManager::IsTimerStop()){
                 auto time1 = now_to_us();
                 while(epoch >= EpochManager::GetPhysicalEpoch()) usleep(logical_sleep_timme);
                 LOG(INFO) << "**** Start Epoch Merge Epoch : " << epoch << "****\n";
+                while(!EpochMessageReceiveHandler::IsShardingACKReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
+                LOG(INFO) << "**** finished IsShardingACKReceiveComplete : " << epoch << "****\n";
+                while(!EpochMessageReceiveHandler::IsShardingSendFinish(epoch)) usleep(logical_sleep_timme);
+                LOG(INFO) << "**** finished IsShardingSendFinish : " << epoch << "****\n";
                 while(!EpochMessageReceiveHandler::CheckEpochShardingSendComplete(ctx, epoch)) usleep(logical_sleep_timme);
                 auto time2 = now_to_us();
                 LOG(INFO) << "**** Finished CheckEpochShardingSendComplete Epoch : " << epoch << ",time cost : " << time2 - time1 << "****\n";
+
+                while(!EpochMessageReceiveHandler::IsShardingPackReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
+                LOG(INFO) << "**** finished IsShardingPackReceiveComplete : " << epoch << "****\n";
+                while(!EpochMessageReceiveHandler::IsShardingTxnReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
+                LOG(INFO) << "**** finished IsShardingTxnReceiveComplete : " << epoch << "****\n";
                 while(!EpochMessageReceiveHandler::CheckEpochShardingReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
                 auto time3 = now_to_us();
                 LOG(INFO) << "**** Finished CheckEpochShardingReceiveComplete Epoch : " << epoch << ",time cost : " << time3 - time2 << "****\n";
+
+                while(!EpochMessageReceiveHandler::IsBackUpACKReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
+                LOG(INFO) << "**** finished IsBackUpACKReceiveComplete : " << epoch << "****\n";
+                while(!EpochMessageReceiveHandler::IsBackUpSendFinish(epoch)) usleep(logical_sleep_timme);
+                LOG(INFO) << "**** finished IsBackUpSendFinish : " << epoch << "****\n";
                 while(!EpochMessageReceiveHandler::CheckEpochBackUpComplete(ctx, epoch)) usleep(logical_sleep_timme);
                 auto time4 = now_to_us();
                 LOG(INFO) << "**** Finished CheckEpochBackUpComplete Epoch : " << epoch << ",time cost : " << time4 - time3 << "****\n";
+
                 while(!Merger::CheckEpochMergeComplete(ctx, epoch)) usleep(logical_sleep_timme);
                 EpochManager::SetShardingMergeComplete(epoch, true);
                 merge_epoch.fetch_add(1);
                 auto time5 = now_to_us();
                 LOG(INFO) << "**** Finished Epoch Merge Epoch : " << epoch << ",time cost : " << time5 - time1 << ",rest time cost : " << time5 - time4 << "****\n";
+
+
                 /// in multi master mode, there is no need to send and merge sharding abort set
+//                while(!EpochMessageReceiveHandler::IsAbortSetACKReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
+//                LOG(INFO) << "**** finished IsAbortSetACKReceiveComplete : " << epoch << "****\n";
+//                while(!EpochMessageReceiveHandler::IsAbortSetReceiveComplete(ctx, epoch)) usleep(logical_sleep_timme);
+//                LOG(INFO) << "**** finished IsAbortSetReceiveComplete : " << epoch << "****\n";
+//                while(!EpochMessageReceiveHandler::CheckEpochAbortSetMergeComplete(ctx, epoch)) usleep(logical_sleep_timme);
                 EpochManager::SetAbortSetMergeComplete(epoch, true);
                 abort_set_epoch.fetch_add(1);
                 auto time6 = now_to_us();
                 LOG(INFO) << "******* Finished Abort Set Merge Epoch : " << epoch << ",time cost : " << time6 - time5 << "********\n";
+
+
                 while(!Merger::CheckEpochCommitComplete(ctx, epoch)) usleep(logical_sleep_timme);
                 EpochManager::SetCommitComplete(epoch, true);
                 commit_epoch.fetch_add(1);
@@ -115,13 +139,14 @@ namespace Taas {
                 auto time7 = now_to_us();
                 auto epoch_commit_success_txn_num = Merger::epoch_record_committed_txn_num.GetCount(epoch);
                 total_commit_txn_num += epoch_commit_success_txn_num;///success
-                LOG(INFO) << PrintfToString("************ 完成一个Epoch的合并 Epoch: %lu, EpochSuccessCommitTxnNum: %lu, EpochCommitTxnNum: %lu ************ ",
+                LOG(INFO) << PrintfToString("************ 完成一个Epoch的合并 Epoch: %lu, EpochSuccessCommitTxnNum: %lu, EpochCommitTxnNum: %lu ************\n",
                                             epoch, epoch_commit_success_txn_num, EpochMessageSendHandler::TotalTxnNum.load() - last_total_commit_txn_num)
-                << "**** Epoch: " << epoch
+                << "**** Time Cost  Epoch: " << epoch
                 << ",Merge time cost : " << time5 - time1
                 << ",Abort Set Merge time cost : " << time6 - time5
                 << ",Commit time cost : " << time7 - time6
-                << "Total Time Cost ****\n" << time7 - time1;
+                << "Total Time Cost ****" << time7 - time1
+                << "****\n";
                 epoch ++;
                 last_total_commit_txn_num = EpochMessageSendHandler::TotalTxnNum.load();
             }
