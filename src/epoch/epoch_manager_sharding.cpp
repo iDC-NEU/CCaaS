e862eb43b9d742c85b0107e1027d95bf0b5a3d76
zwx-vm2 <745470728@qq.com>
2023-08-17 16:00:04 +0800

    debug tikv error

--- src/epoch/epoch_manager_sharding.cpp
+++ src/epoch/epoch_manager_sharding.cpp
@@ -141,15 +141,16 @@ namespace Taas {
                 auto time7 = now_to_us();
                 auto epoch_commit_success_txn_num = Merger::epoch_record_committed_txn_num.GetCount(epoch);
                 total_commit_txn_num += epoch_commit_success_txn_num;///success
-                LOG(INFO) << PrintfToString("************ 完成一个Epoch的合并 Epoch: %lu, Local EpochSuccessCommitTxnNum: %lu,TotalSuccessTxnNum: %lu, EpochCommitTxnNum: %lu ",
-                                            epoch, epoch_commit_success_txn_num, total_commit_txn_num,
-                                            EpochMessageSendHandler::TotalTxnNum.load() - last_total_commit_txn_num)
-                          << ",Time Cost  Epoch: " << epoch
-                          << ",Merge time cost : " << time5 - time1
-                          << ",Abort Set Merge time cost : " << time6 - time5
-                          << ",Commit time cost : " << time7 - time6
-                          << "Total Time Cost ****" << time7 - time1
-                          << "****\n";
+                if(epoch % ctx.print_mode_size == 0)
+                    LOG(INFO) << PrintfToString("************ 完成一个Epoch的合并 Epoch: %lu, Local EpochSuccessCommitTxnNum: %lu,TotalSuccessTxnNum: %lu, EpochCommitTxnNum: %lu ",
+                                                epoch, epoch_commit_success_txn_num, total_commit_txn_num,
+                                                EpochMessageSendHandler::TotalTxnNum.load() - last_total_commit_txn_num)
+                              << ",Time Cost  Epoch: " << epoch
+                              << ",Merge time cost : " << time5 - time1
+                              << ",Abort Set Merge time cost : " << time6 - time5
+                              << ",Commit time cost : " << time7 - time6
+                              << "Total Time Cost ****" << time7 - time1
+                              << "****\n";
                 epoch ++;
                 last_total_commit_txn_num = EpochMessageSendHandler::TotalTxnNum.load();
             }
