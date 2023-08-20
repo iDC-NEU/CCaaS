a1cbf9422fd4c4315d4741ba0ca9036116a907e3
zwx-vm2 <745470728@qq.com>
2023-08-20 22:50:36 +0800

    reduce thread contention

--- src/transaction/crdt_merge.cpp
+++ src/transaction/crdt_merge.cpp
@@ -9,30 +9,31 @@
 namespace Taas {
     bool CRDTMerge::ValidateReadSet(const Context &ctx, std::shared_ptr<proto::Transaction> txn_ptr) {
         ///RC & RR & SI
-        auto epoch_mod = txn_ptr->commit_epoch() % ctx.kCacheMaxLength;
-        std::string key, version;
-        uint64_t csn = 0;
-        for(auto i = 0; i < txn_ptr->row_size(); i ++) {
-            const auto& row = txn_ptr->row(i);
-            if(row.op_type() != proto::OpType::Read) {
-                continue;
-            }
-            /// indeed, we should use the csn to check the read version,
-            /// but there are some bugs in updating the csn to the storage(tikv).
-            if (!Merger::read_version_map_data.getValue(row.key(), version)) {
-                /// should be abort, but Taas do not connect load data,
-                /// so read the init snap will get empty in read_version_map
-                continue;
-            }
-            if (version != row.data()) {
-                auto csn_temp = std::to_string(txn_ptr->csn()) + ":" + std::to_string(txn_ptr->server_id());
-                Merger::epoch_abort_txn_set[epoch_mod]->insert(csn_temp, csn_temp);
-                Merger::local_epoch_abort_txn_set[epoch_mod]->insert(csn_temp, csn_temp);
-//                LOG(INFO) <<"Txn read version check failed";
-//                LOG(INFO) <<"read version check failed version : " << version << ", row.data() : " << row.data();
-                return false;
-            }
-        }
+        //RC do not check read data
+//        auto epoch_mod = txn_ptr->commit_epoch() % ctx.kCacheMaxLength;
+//        std::string key, version;
+//        uint64_t csn = 0;
+//        for(auto i = 0; i < txn_ptr->row_size(); i ++) {
+//            const auto& row = txn_ptr->row(i);
+//            if(row.op_type() != proto::OpType::Read) {
+//                continue;
+//            }
+//            /// indeed, we should use the csn to check the read version,
+//            /// but there are some bugs in updating the csn to the storage(tikv).
+//            if (!Merger::read_version_map_data.getValue(row.key(), version)) {
+//                /// should be abort, but Taas do not connect load data,
+//                /// so read the init snap will get empty in read_version_map
+//                continue;
+//            }
+//            if (version != row.data()) {
+//                auto csn_temp = std::to_string(txn_ptr->csn()) + ":" + std::to_string(txn_ptr->server_id());
+//                Merger::epoch_abort_txn_set[epoch_mod]->insert(csn_temp, csn_temp);
+//                Merger::local_epoch_abort_txn_set[epoch_mod]->insert(csn_temp, csn_temp);
+////                LOG(INFO) <<"Txn read version check failed";
+////                LOG(INFO) <<"read version check failed version : " << version << ", row.data() : " << row.data();
+//                return false;
+//            }
+//        }
         return true;
     }
 
