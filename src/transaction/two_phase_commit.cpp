27c048a9af4b995daf3d4af1f01182dd9edc79ff
zwx-vm2 <745470728@qq.com>
2023-09-18 11:09:27 +0800

    2PC

--- src/transaction/two_phase_commit.cpp
+++ src/transaction/two_phase_commit.cpp
@@ -160,24 +160,24 @@ namespace Taas {
 
   // 处理接收到的消息
   bool TwoPC::HandleReceivedMessage() {
-    if (MessageQueue::listen_message_queue->try_dequeue(message_ptr)) {
-      if (message_ptr->empty()) return false;
-      message_string_ptr = std::make_unique<std::string>(
-          static_cast<const char*>(message_ptr->data()), message_ptr->size());
-      msg_ptr = std::make_unique<proto::Message>();
-      res = UnGzip(msg_ptr.get(), message_string_ptr.get());
-      assert(res);
-      if (msg_ptr->type_case() == proto::Message::TypeCase::kTxn) {
-        txn_ptr = std::make_unique<proto::Transaction>(*(msg_ptr->release_txn()));
-        SetMessageRelatedCountersInfo();
-        HandleReceivedTxn();
-      } else {
-        MessageQueue::request_queue->enqueue(std::move(msg_ptr));
-        MessageQueue::request_queue->enqueue(nullptr);
+      while(!EpochManager::IsTimerStop()) {
+          MessageQueue::listen_message_queue->wait_dequeue(message_ptr);
+          if (message_ptr == nullptr || message_ptr->empty()) continue;
+          message_string_ptr = std::make_unique<std::string>(
+                  static_cast<const char*>(message_ptr->data()), message_ptr->size());
+          msg_ptr = std::make_unique<proto::Message>();
+          res = UnGzip(msg_ptr.get(), message_string_ptr.get());
+          assert(res);
+          if (msg_ptr->type_case() == proto::Message::TypeCase::kTxn) {
+              txn_ptr = std::make_unique<proto::Transaction>(*(msg_ptr->release_txn()));
+              SetMessageRelatedCountersInfo();
+              HandleReceivedTxn();
+          } else {
+              MessageQueue::request_queue->enqueue(std::move(msg_ptr));
+              MessageQueue::request_queue->enqueue(nullptr);
+          }
       }
       return true;
-    }
-    return false;
   }
 
   // 核心：根据txn type操作
