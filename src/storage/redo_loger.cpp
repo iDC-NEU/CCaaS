0be2b0a75b88e2f6bffef3f216398c4f83582369
singhear <xinjiempolde@qq.com>
2023-09-21 22:07:20 +0800

    fix nebula bug

--- src/storage/redo_loger.cpp
+++ src/storage/redo_loger.cpp
@@ -32,6 +32,7 @@ namespace Taas {
             HBase::StaticInit(ctx);
         }
         MOT::StaticInit(ctx);
+        Nebula::StaticInit(ctxn);
     }
 
     void RedoLoger::ClearRedoLog(uint64_t& epoch) {
